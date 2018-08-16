431bfbdd49d5bc0a74c95689e6bb617bf99c5efb
neonwednesdays <dxiao.ns@gmail.com>
2018-08-16 16:55:38 -0700

    first pass at styling and making news component

--- app/src/pages/HomePage.js
+++ app/src/pages/HomePage.js
@@ -2,18 +2,17 @@ import React, { Component } from 'react';
 
 import { Link } from 'components/ButtonComponent';
 import Layout, { Panel, InnerPanel } from 'components/LayoutComponent';
+import NewsPanel from 'panels/NewsPanel';
 
 class HomePage extends Component {
   render() {
     return (
-      <Layout className="tg-home tg-page">
+      <Layout className='tg-home tg-page'>
         <Panel>
           test
         </Panel>
 
-        <InnerPanel isGray>
-          Development Notes!
-        </InnerPanel>
+        <NewsPanel />
       </Layout>
     );
   }
---
+++ app/src/panels/NewsPanel.js
@@ -0,0 +1,44 @@
+import React, { PureComponent } from 'react';
+import { InnerPanel } from 'components/LayoutComponent';
+
+
+class NewsItem extends PureComponent {
+  static defaultProps = {
+    data: {},
+  };
+
+  render() {
+    const { data: { date, content, title } } = this.props;
+
+    return (
+      <li className='tg-news-item'>
+        <span className='tg-news-item-title'>{ title }</span>
+        <span className='tg-news-item-date'>{ date }</span>
+        <p className='tg-news-item-content'>{ content }</p>
+      </li>
+    )
+  }
+}
+
+class NewsPanel extends PureComponent {
+  static defaultProps = {
+    newsData: [{date: 'Thu Aug 16 2018 16:37:57 GMT-0700 (Pacific Daylight Time)', title: 'Start of News', content: 'Together Quest ~ begins together!'}],
+  };
+
+  render() {
+    const { newsData } = this.props;
+
+    return (
+      <InnerPanel className='tg-news'>
+        <h2 className='tg-news-heading'>News</h2>
+        <ul className='tg-news-list'>
+          { newsData.map((news) => {
+            return <NewsItem data={news} key={news.date}/>
+          })}
+        </ul>
+      </InnerPanel>
+    );
+  }
+}
+
+export default NewsPanel;
