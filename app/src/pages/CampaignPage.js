bafc6c3300a1eb57a8f5fe784df9f88406cd4904
neonwednesdays <dxiao.ns@gmail.com>
2018-08-17 00:53:50 -0700

    hook up campaign data

--- app/src/pages/CampaignPage.js
+++ app/src/pages/CampaignPage.js
@@ -14,26 +14,63 @@ import {
 
 import CharacterData from 'data/CharacterData';
 
-class CampaignSelectPanel extends PureComponent {
-  render() {
-    return (
-      <Panel>
-        <h2>Campaigns</h2>
-
-        <Panel inner className='bg-gray'>
-          <Link to='/new-campaign'><Icon name='fa-map-marked-alt' /><span>Create a new Campaign</span></Link>
-        </Panel>
-
-        <Panel inner className='bg-gray'>
-          <ButtonGroup>
-            <Button>Cat Quest</Button>
-          </ButtonGroup>
-        </Panel>
-
-      </Panel>
-    )
-  }
+// redux mappings
+function mapStateToProps(state) {
+  return {
+    campaigns: state.campaigns,
+  };
 };
+
+function mapDispatchToProps(dispatch) {
+  return {};
+};
+
+const ConnectedCampaignSelectPanel = connect(
+  mapStateToProps, mapDispatchToProps
+)(
+  class CampaignSelectPanel extends PureComponent {
+    static defaultProps = {
+      campaigns: [],
+      onCampaignSelect: () => {},
+    }
+    render() {
+      const { campaigns } = this.props;
+
+      return (
+        <Panel>
+          <h2>Campaigns</h2>
+
+          <Panel inner className='bg-gray'>
+            <Link to='/new-campaign'><Icon name='fa-map-marked-alt' /><span>Create a new Campaign</span></Link>
+          </Panel>
+
+          <Panel inner className='bg-gray'>
+            <ButtonGroup>
+              { campaigns.map((campaign) => {
+                const { title } = campaign;
+
+                return (
+                  <Button
+                    key={title}
+                    onClick={this.handleOnCampaignClick}
+                  >
+                    {title}
+                  </Button>
+                )
+              })}
+            </ButtonGroup>
+          </Panel>
+
+        </Panel>
+      )
+    }
+
+    handleOnCampaignClick = (campaign) => {
+      const { onCampaignSelect } = this.props;
+      onCampaignSelect(this.props.campaigns[0]);
+    }
+  }
+);
 
 class CampaignDetailsPanel extends PureComponent {
   static defaultProps = {
@@ -64,55 +101,49 @@ class CampaignDetailsPanel extends PureComponent {
   }
 }
 
-class CampaignPage extends Component {
-  state = {
-    activePanel: 1,
-    selectedCharacter: undefined,
-    selectedCampaign: undefined,
-  }
-
-  render() {
-    const { activePanel, selectedCampaign, selectedCharacter } = this.state;
-
-    return (
-      <Layout multi activePanel={activePanel} className='tg-campaign tg-page'>
-
-        <CampaignSelectPanel />
-
-        <CampaignDetailsPanel data={selectedCampaign} />
-
-        <Panel className='tg-character-panel'>
-          { selectedCharacter &&
-            <CharacterComponent character={selectedCharacter} />
-          }
-        </Panel>
-
-      </Layout>
-    );
-  }
-
-  handleSelectCharacter = (character) => {
-    this.setState({
-      selectedCharacter: character,
-    })
-  }
-}
-
-
-// connect component to State
-function mapStateToProps(state) {
-  return {
-    campaigns: state.campaigns,
-  };
-};
-
-function mapDispatchToProps(dispatch) {
-  return {};
-};
-
-// export component
-const CampaignPageConnected = connect(
+const ConnectedCampaignPage = connect(
   mapStateToProps, mapDispatchToProps
-)(CampaignPage)
+)(
+  class CampaignPage extends Component {
+    state = {
+      activePanel: 1,
+      selectedCharacter: undefined,
+      selectedCampaign: undefined,
+    }
 
-export default CampaignPageConnected;
+    render() {
+      const { activePanel, selectedCampaign, selectedCharacter } = this.state;
+
+      return (
+        <Layout multi activePanel={activePanel} className='tg-campaign tg-page'>
+
+          <ConnectedCampaignSelectPanel onCampaignSelect={this.handleOnCampaignSelect} />
+
+          <CampaignDetailsPanel data={selectedCampaign} />
+
+          <Panel className='tg-character-panel'>
+            { selectedCharacter &&
+              <CharacterComponent character={selectedCharacter} />
+            }
+          </Panel>
+
+        </Layout>
+      );
+    }
+
+    handleOnCampaignSelect = (campaign) => {
+      this.setState({
+        activePanel: 2,
+        selectedCampaign: campaign,
+      })
+    }
+
+    handleSelectCharacter = (character) => {
+      this.setState({
+        selectedCharacter: character,
+      })
+    }
+  }
+);
+
+export default ConnectedCampaignPage;
