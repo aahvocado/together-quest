e9a4e825984d2240442c777855353cb5ed07ed5b
aahvocado <dan.dan.makes.stuff@gmail.com>
2018-11-11 21:17:22 -0800

    webpack and babel setup for server

--- server/src/server.js
+++ server/src/server.js
@@ -1,3 +1,5 @@
+import "@babel/polyfill";
+
 import express from 'express';
 import http from 'http';
 
@@ -7,7 +9,7 @@ const SERVER_PORT = 1111;
 
 const app = express();
 app.get('/health', function(req, resp) {
-  resp.send('EventApp is running');
+  resp.send('server is okay');
 });
 
 const server = http.createServer(app);
@@ -16,4 +18,5 @@ const io = new SocketServer(server);
 // START!
 server.listen(SERVER_PORT, async () => {
   await io.start();
+  console.log('\x1b[36m', 'TogetherQuest Server Started');  //cyan
 });
---
+++ server/webpack.config.js
@@ -0,0 +1,37 @@
+var path = require('path');
+var webpack = require('webpack');
+var nodeExternals = require('webpack-node-externals');
+
+// we have two separate webpack files because the Node.js has its own `require` module,
+// meaning it doesn't require external `node_modules`
+// but the web browser needs those external modules since it has nothing
+const serverPath = path.resolve(__dirname, './src/');
+module.exports = {
+  mode: 'development',
+  target: 'node',
+  entry: './src/server.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'server.js'
+  },
+  resolve: {
+    alias: {
+      components: path.resolve(serverPath, 'components'),
+    }
+  },
+  externals: [nodeExternals()],
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+};
